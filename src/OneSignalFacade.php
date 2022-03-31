fa1d6746c4023a32b8263b0e86801fb713772a7b
karunais <karunais1392@gmail.com>
2022-03-31 14:38:39 +0800

    added proxy support

---
+++ src/OneSignalFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Karu\OneSignal;
+
+use Illuminate\Support\Facades\Facade;
+
+class OneSignalFacade extends Facade {
+
+    protected static function getFacadeAccessor() {
+        return 'onesignal';
+    }
+
+}
